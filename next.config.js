d960cd12e216e2663d5c9d1f202c7896ad003aaf
ankush357159 <ankush357159@gmail.com>
2021-10-07 01:40:06 +0530

    map functionality

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    images: {
-        domains: ['links.papareact.com']
-    }
-}
+	images: {
+		domains: ["links.papareact.com"],
+	},
+	env: {
+		mapbox_key:
+			"pk.eyJ1IjoiYW5rdXNoMzU3MTU5IiwiYSI6ImNrdWZ0N2R2dDAzMmMydXF2Nnk1aDZ6amoifQ.yulwd0s4X7ySdpk_b5fuhg",
+	},
+};
